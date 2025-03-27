af01a320b794ad1523bed409863d14afbd131238
Anton.Teroshyn <ateroshyn@grubhub.com>
2025-03-27 15:27:32 +0100

    Spring initilzr

---
+++ src/main/java/com/olive/ateroshyn_test/AteroshynTestApplication.java
@@ -0,0 +1,13 @@
+package com.olive.ateroshyn_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteroshynTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteroshynTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olive/ateroshyn_test/AteroshynTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olive.ateroshyn_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AteroshynTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
